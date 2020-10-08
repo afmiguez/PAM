f04bb10517ffb6588eb35c318c1edfa99237488d
Ines <ines.capela@infolabix.com>
2020-10-08 19:22:48 +0100

    add imports

--- app/src/main/java/edu/ufp/pam/examples/navdraw/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/edu/ufp/pam/examples/navdraw/ui/gallery/GalleryFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import edu.ufp.pam.examples.navdraw.R
+import com.example.pamandroidkotlin.R
 
 class GalleryFragment : Fragment() {
 
